946e4034a19a55cad5ea88febf8c13cd06ce3250
Irham Naufal <inaufal77@gmail.com>
2024-09-16 23:09:21 +0700

    feature: init front-end base structure

--- frontend/theatr/src/app/app.routes.ts
+++ frontend/theatr/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { RouterConfig } from './config/app.constants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: RouterConfig.LOGIN.path,
+        loadChildren: () =>
+            import('./pages/login/login.routes').then((m) => m.loginRoutes),
+        title: RouterConfig.LOGIN.title,
+        data: RouterConfig.LOGIN.data,
+    },
+];
