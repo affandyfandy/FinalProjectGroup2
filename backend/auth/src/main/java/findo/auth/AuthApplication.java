1ccff3412ee407795b91cf4790ea50c8538464b1
Chandra <ryan.chandra.hadi@gmail.com>
2024-09-18 23:33:00 +0700

    feature: merge from dev

--- backend/auth/src/main/java/findo/auth/AuthApplication.java
+++ backend/auth/src/main/java/findo/auth/AuthApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {
-    org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration.class,
-    org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration.class
+		org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration.class
 })
 @EnableDiscoveryClient
 public class AuthApplication {
