6706bdcb8a131b5957746022485d680f4d161a0c
Irham Naufal <inaufal77@gmail.com>
2024-09-21 01:37:37 +0700

    feature: implement customer schedule UI

---
+++ frontend/theatr/src/app/pages/schedules/schedules.route.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+import { MovieScheduleComponent } from "./movie-schedule/movie-schedule.component";
+import { BookScheduleComponent } from "./book-schedule/book-schedule.component";
+import { accessGuard } from "../../main/guards/access.guard";
+
+export const scheduleRoutes: Routes = [
+    { path: ':id', component: MovieScheduleComponent },
+    {
+        path: ':id/book',
+        data: { roles: ['ROLE_CUSTOMER'] },
+        canActivate: [accessGuard],
+        component: BookScheduleComponent
+    }
+]
